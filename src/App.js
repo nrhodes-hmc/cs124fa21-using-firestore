587ff30591e8f8f9022867c3e454a7c15b8be497
Neil Rhodes <neil@pobox.com>
2021-10-20 13:45:22 -0700

    Switch to using Firestore

--- src/App.js
+++ src/App.js
@@ -2,55 +2,60 @@ import './App.css';
 
 import People from './People';
 
-import { useState } from 'react';
 import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
+import firebase from "firebase/compat";
+import {useCollection} from "react-firebase-hooks/firestore";
 
-const initialData = [
-    {
-        id: "1276",
-        name: "Neil Rhodes",
-        email: "rhodes@hmc.edu",
-        phone: "(909) 555-1212"
-    },
-    {
-        id: "787",
-        name: "Barack Obama",
-        email: "ex-prez@whitehouse.gov",
-        phone: "(312) 555-1212"
-    }
-];
+const firebaseConfig = {
+    apiKey: "AIzaSyCcQ6XCOvMIA7pHME4bWBgy_7OVy_7XErA",
+    authDomain: "cs124-fall2021.firebaseapp.com",
+    projectId: "cs124-fall2021",
+    storageBucket: "cs124-fall2021.appspot.com",
+    messagingSenderId: "264318304667",
+    appId: "1:264318304667:web:4be8d27a02811b1ccd613e"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
-
-
+const collectionName = "People-XXX"
 function App() {
-    const [data, setData] = useState(initialData);
+    const query = db.collection(collectionName);
+    const [value, loading, error] = useCollection(query);
+
+    let people = null;
+    if (value) {
+        people = value.docs.map((doc) => {
+             return {...doc.data()}});
+    }
 
     function handleDeletePerson(personId) {
-        setData(data.filter(person => person.id !==personId))
+        db.collection(collectionName).doc(personId).delete();
     }
 
     function handleAddPerson() {
-        setData([...data, {
-            id: generateUniqueID(),
-            name: "",
-            email: "",
-            phone: ""
-        }])
+        const newId = generateUniqueID();
+        db.collection(collectionName).doc(newId).set({
+            id: newId,
+            title: "",
+        })
     }
 
     function handlePersonFieldChanged(personId, field, value) {
-        setData(data.map(
-            person => person.id !==personId
-                ? person
-                : {...person, [field]: value}))
+        const doc = db.collection(collectionName).doc(personId);
+        doc.update({
+            [field]: value,
+        })
     }
 
     return <div>
-        <People list={data}
-                onDeletePerson={handleDeletePerson}
-                onAddPerson={handleAddPerson}
-                onPersonFieldChanged={handlePersonFieldChanged}
-        /></div>;
+        {loading && <h1>Loading</h1>}
+        {people && <People list={people}
+                          onDeletePerson={handleDeletePerson}
+                          onAddPerson={handleAddPerson}
+                          onPersonFieldChanged={handlePersonFieldChanged}
+        />
+        }
+    </div>;
 }
 
 export default App;
